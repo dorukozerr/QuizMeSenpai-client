29df42102ad4736be5f8279e09bc5d9f6e129ff0
Doruk Özer <dorukozer@protonmail.com>
2024-12-23 08:13:07 +0300

    feat: expo router and tamagui setup

---
+++ src/app/_layout.tsx
@@ -0,0 +1,24 @@
+import { Slot } from "expo-router";
+import { useColorScheme, SafeAreaView } from "react-native";
+import { TamaguiProvider } from "tamagui";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+
+import { tamaguiConfig } from "../../tamagui.config";
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <SafeAreaView>
+          <Slot />
+        </SafeAreaView>
+      </ThemeProvider>
+    </TamaguiProvider>
+  );
+}
